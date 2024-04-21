f7c68dcb202f46155de8fa864824fdda06e147dd
Vishal Jadhav <143590501+vishaljadhav7@users.noreply.github.com>
2024-04-21 11:29:07 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,30 +1,20 @@
-const accordionItems = document.querySelectorAll('.accordion-item');
+const items = document.querySelectorAll('.accordion-item');
 
-accordionItems.forEach(item => {
+items.forEach(item =>{
+  
   const title = item.querySelector('.accordion-title');
-  const content = item.querySelector('.accordion-content');
-
-  title.addEventListener('click', () => {
-    for (i = 0; i < accordionItems.length; i++) {
-      if(accordionItems[i] != item){
-        accordionItems[i].classList.remove('active');
+  // const content = item.querySelector('.accordion-content');
+  
+  title.addEventListener('click', ()=>{
+    
+    for(let i = 0 ; i < items.length ; i++){
+      if(items[i]!=item){
+         items[i].classList.remove("active");
       }else{
-        // toggle the accordion item
-        item.classList.toggle('active');
-      }
+        item.classList.toggle("active");
+      } 
+      
     }
-
-  });
-});
-
-
-
-// // JavaScript for event delegation
-// document.getElementById('accordion').addEventListener('click', function(event) {
-//   // Check if the clicked element is an accordion title
-//   if (event.target.classList.contains('accordion-title')) {
-//     // Toggle the 'active' class on the content element
-//     var content = event.target.nextElementSibling;
-//     content.classList.toggle('active');
-//   }
-// });
+  })
+  
+})
